04beed4b58397f3dd1c83a824341007331b395cc
puspa thapa magar <avishkartamang@gmail.com>
2021-08-10 20:51:46 -0400

    models added

--- FIRSTAPPLICATION/models.py
+++ FIRSTAPPLICATION/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50,help_text="Enter the name of the project")
+    Description = models.TextField(max_length=1000,help_text="Enter the description")
+    back_end_technology = models.CharField(max_length=50,verbose_name = "Backend Technology")
+    front_end_technology = models.CharField(max_length=50,verbose_name="Frontend Technology")
