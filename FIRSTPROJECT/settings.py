cb487674b2ff538c9a5e3d5c99c52352081847b4
puspa thapa magar <avishkartamang@gmail.com>
2021-08-10 21:04:56 -0400

    more heroku config for static files

--- FIRSTPROJECT/settings.py
+++ FIRSTPROJECT/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "FIRSTAPPLICATION.apps.FirstapplicationConfig",
 ]
@@ -130,5 +131,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 ALLOWED_HOSTS = ['*']
