8facf4d368e9cfa6633d71e6ec9ce20c0897aafa
puspa thapa magar <avishkartamang@gmail.com>
2021-08-10 21:21:01 -0400

    settings file debugged

--- FIRSTPROJECT/settings.py
+++ FIRSTPROJECT/settings.py
@@ -132,5 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ALLOWED_HOSTS = ['*']
